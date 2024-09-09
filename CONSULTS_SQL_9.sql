6906efd89e1b1c4d9206f785867d1d9f93d46975
fgarciacp <fgarciacp1@gmail.com>
2024-09-08 19:48:22 -0600

    sql uno

---
+++ CONSULTS_SQL_9.sql
@@ -0,0 +1,38 @@
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees e
+JOIN salaries s ON e.emp_no = s.emp_no;
+
+SELECT e.first_name, e.last_name, e.hire_date
+FROM employees e
+WHERE EXTRACT(YEAR FROM e.hire_date) = 1986;
+
+SELECT d.dept_name, d.dept_no, e.emp_no, e.last_name, e.first_name
+FROM departments d
+JOIN dept_manager dm ON d.dept_no = dm.dept_no
+JOIN employees e ON dm.emp_no = e.emp_no;
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no;
+
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hércules' AND last_name LIKE 'B%';
+
+SELECT e.emp_no, e.last_name, e.first_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Ventas';
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Ventas', 'Desarrollo');
+
+SELECT last_name, COUNT(last_name) AS frequency
+FROM employees
+GROUP BY last_name
+ORDER BY frequency DESC;
